9a145a503f9bc0626be0d9d9291532f8b9775ef1
omer <96634886+omerzdn@users.noreply.github.com>
2026-01-29 00:40:29 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-  </head>
-  <body>
-    paranızı ödeyin kanka millet osbir çekemiyor :)
-  </body>
-</html>
-`;
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+
+const html = `paranızı ödeyin kanka millet osbir çekemiyor :)`;
+
+app.get("/", (req, res) => res.type('html').send(html));
+
+const server = app.listen(port, () =>
+  console.log(`Example app listening on port ${port}!`)
+);
+
+server.keepAliveTimeout = 120 * 1000;
+server.headersTimeout = 120 * 1000;
